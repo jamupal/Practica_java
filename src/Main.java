9dfb41da17804bbe0222d5edbac364f27bfb05b6
Hamilton Murillo Palacio <hamilton.murillo@landsoft.com.co>
2022-10-19 15:44:03 -0500

    tema_4 - Ejercicios condicionales

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        Tema4 tems4 = new Tema4();
+
         System.out.println("The sum of the numbers is: " + addNumbers(5 , 4 , 9));
 
         Car car = new Car();
         car.getDoor();
         System.out.println("The number of doors is: " + car.door);
+
+        //Condicionales
+        tems4.ifTest(747);
+        tems4.whileTest(0);
+        tems4.doWhileTest(4);
+        tems4.forTest();
+        tems4.switchTest("Cama");
     }
     public static int addNumbers(int number1, int number2, int number3){
         return number1 + number2 + number3;
---
+++ src/Tema4.java
@@ -0,0 +1,59 @@
+public class Tema4 {
+
+    public void ifTest(int numberIf){
+        System.out.println("\nInicio If \n");
+        if(numberIf > 0){
+            System.out.println("El número ingresado es positivo");
+        } else if (numberIf < 0) {
+            System.out.println("El numero ingresado es negativo");
+        }else {
+            System.out.println("El numero ingresado es 0");
+        }
+    }
+
+    public void whileTest(int numeroWhile){
+        System.out.println("\nInicio ciclo while \n");
+        while (numeroWhile < 3){
+            System.out.println("El valor que tiene la variable es "+ numeroWhile);
+            numeroWhile ++;
+        }
+    }
+
+    public void  doWhileTest(int numeroWhile){
+        System.out.println("\nInicio ciclo do while \n");
+        do {
+            System.out.println("El valor que tiene la variable es "+ numeroWhile);
+        }while (false);
+    }
+
+    public void forTest(){
+        System.out.println("\nInicio ciclo for \n");
+        for (int numeroFor = 0; numeroFor < 3; numeroFor ++){
+            System.out.println("El valor de numeroFor es: "+numeroFor);
+        }
+    }
+
+    public void switchTest(String season){
+        System.out.println("\nInicio ciclo switch \n");
+        switch (season.toLowerCase()){
+            case "spring":
+                System.out.println("The season entered is "+ season);
+                break;
+            case "fall":
+                System.out.println("The season entered is "+ season);
+                break;
+            case "winter":
+                System.out.println("The season entered is "+ season);
+                break;
+            case "summer":
+                System.out.println("The season entered is  "+ season);
+                break;
+            default:
+                System.out.println("The word entered is not a season: " + season);
+
+        }
+
+    }
+
+
+}
