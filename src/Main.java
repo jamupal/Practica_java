34b68da13a740c20f1636aa946ac381e813843c9
Hamilton Murillo Palacio <hamilton.murillo@landsoft.com.co>
2022-10-25 20:31:36 -0500

    tema_9 - Se añade requerientos solicitados en la actividad del tema 9

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("The sum of the numbers is: " + addNumbers(5 , 4 , 9));
+        Cliente cliente = new Cliente();
+        Trabajador worker = new Trabajador();
+        //Seccion cliente
+        cliente.setAge(17);
+        cliente.setPhone("5795879");
+        cliente.setName("Manuel Cruz");
+        cliente.setCredito(280000);
+        //Seccion trabajador
+        worker.setAge(37);
+        worker.setPhone("58794565");
+        worker.setName("Pedro Caballero");
+        worker.setSalary(450000);
 
-        Car car = new Car();
-        car.getDoor();
-        System.out.println("The number of doors is: " + car.door);
+        System.out.println("El nombre del cliente es: " + cliente.getName());
+        System.out.println("La edad de la persona es: " + cliente.getAge());
+        System.out.println("El telefono de la persona es: " + cliente.getPhone());
+        System.out.println("La persona tiene un credito de: " + cliente.getCredito() + "\n");
+
+        System.out.println("El nombre del Trabajador es: " + worker.getName());
+        System.out.println("La edad de la persona es: " + worker.getAge());
+        System.out.println("El telefono de la persona es: " + worker.getPhone());
+        System.out.println("La persona tiene un salario de: " + worker.getSalary());
     }
     public static int addNumbers(int number1, int number2, int number3){
         return number1 + number2 + number3;
---
+++ src/Trabajador.java
@@ -0,0 +1,11 @@
+public class Trabajador extends Persona{
+    private int salary;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
